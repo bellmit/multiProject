d000b11bc503b4eeb1457bfa0224b7b41b291cfd
Unknown <ruudvdboomen@hotmail.com>
2019-04-02 14:28:17 +0200

    Folder structure change and naming fix

--- login/src/main/java/dao/interfaces/BaseDao.java
+++ login/src/main/java/dao/interfaces/BaseDao.java
@@ -1,11 +1,11 @@
 package dao.interfaces;
 
 public interface BaseDao<T> {
-    void create(T Object);
+    void create(T object);
 
     T find(String id);
 
-    void edit(T Object);
+    void edit(T object);
 
-    void delete(T Object);
+    void delete(T object);
 }
