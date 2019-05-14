0ea2b8707c7ee9af3323fb3b6c277adfb917f164
Damhuis <ldamhuis@hotmail.com>
2019-05-14 15:25:57 +0200

    Added reservation timer

---
+++ reserve/src/main/java/Util/Mailer.java
@@ -0,0 +1,35 @@
+package Util;
+
+import javax.annotation.Resource;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.Serializable;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Mailer implements Serializable {
+
+    @Resource(name = "java:jboss/mail/gmail")
+    private Session session;
+
+    public String send(String addresses, String topic, String textMessage) {
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(addresses));
+            message.setSubject(topic);
+            message.setText(textMessage);
+
+            Transport.send(message);
+
+        } catch (MessagingException e) {
+            Logger.getLogger(Mailer.class.getName()).log(Level.WARNING, "Cannot send mail", e);
+        }
+        return  "succes";
+    }
+
+
+}
