c9f7ec28a383da8bef72c2c12206ff01f1b099ca
Unknown <ruudvdboomen@hotmail.com>
2019-05-19 17:00:55 +0200

    Change scheduler code

--- reserve/src/main/java/Util/Scheduler.java
+++ reserve/src/main/java/Util/Scheduler.java
@@ -1,19 +1,14 @@
 package Util;
 
-import DAO.Interfaces.ReservationDAO;
 import DAO.JPA.ReservationDAOJPA;
 import Domain.Reservation;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
-import javax.ejb.Timeout;
-import javax.ejb.TimerService;
+import javax.ejb.*;
 import java.util.ArrayList;
 import java.util.Date;
-import javax.ejb.TimerConfig;
 
 
 @Startup
@@ -28,35 +23,30 @@ public class Scheduler {
     @PostConstruct
     public void initialize() {
         ReservationDAOJPA reservationDAOJPA = new ReservationDAOJPA();
-        ArrayList<Reservation> reservations = reservationDAOJPA.getReservations();
         try {
-            if(reservations.size()!=0){
-                for (Reservation r:reservations) {
-                    this.setNewScheduler(r);
-                }
+            ArrayList<Reservation> reservations = reservationDAOJPA.getReservations();
+            for (Reservation r : reservations) {
+                this.setNewScheduler(r);
             }
-        }  catch (Exception e){
-
+        } catch (Exception e) {
         }
-
-
     }
 
-    public void setNewScheduler(Reservation r){
+    public void setNewScheduler(Reservation r) {
         TimerConfig timerConfig = new TimerConfig();
         timerConfig.setInfo("MailTimer");
         Date now = new Date();
         Date reservationtime = r.getTimeSlots().get(0).getStartTime();
         long difference = reservationtime.getTime() - now.getTime();
-        long x = difference - (10800*1000);
+        long x = difference - (10800 * 1000);
         reser = r;
-        timerService.createSingleActionTimer(x,timerConfig);
+        timerService.createSingleActionTimer(x, timerConfig);
     }
 
     @Timeout
-    public void afterTimeOut(){
+    public void afterTimeOut() {
         Mailer m = new Mailer();
-        m.send(String.valueOf(reser.getUserID()),"Reservation Notification",
+        m.send(String.valueOf(reser.getUserID()), "Reservation Notification",
                 "Dear sir or madam we'd like to inform you that your reservation will start in 3 hours at " + reser.getTimeSlots().get(0).getStartTime().toString());
     }
 
