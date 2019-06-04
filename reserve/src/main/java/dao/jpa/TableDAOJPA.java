4382dfa6e158601f73c11374c67dc46a41a64be2
Unknown <ldamhuis@hotmail.com>
2019-06-04 14:59:19 +0200

    Changed a endpoint

--- reserve/src/main/java/dao/jpa/TableDAOJPA.java
+++ reserve/src/main/java/dao/jpa/TableDAOJPA.java
@@ -3,13 +3,11 @@ package dao.jpa;
 
 import dao.Interfaces.TableDAO;
 import domain.DiningTable;
-import domain.Reservation;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import java.util.ArrayList;
 import java.util.List;
 
 @Stateless
@@ -23,9 +21,9 @@ public class TableDAOJPA extends BaseDaoJpa<DiningTable> implements TableDAO {
     }
 
     @Override
-    public List<DiningTable> getTables() {
-        Query q = em.createQuery("SELECT t from DinningTable t");
-        return new ArrayList<>(q.getResultList());
+    public List getTables() {
+        Query q = em.createQuery("SELECT t from DinningTable t",DiningTable.class);
+        return q.getResultList();
     }
 
 
--- reserve/src/main/java/rest/Rest.java
+++ reserve/src/main/java/rest/Rest.java
@@ -5,16 +5,16 @@ import io.swagger.jaxrs.config.BeanConfig;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/api")
+@ApplicationPath("api")
 public class Rest extends Application {
     public Rest() {
-        BeanConfig beanConfig = new BeanConfig();
+        /*BeanConfig beanConfig = new BeanConfig();
         beanConfig.setVersion("2.0.0");
         beanConfig.setSchemes(new String[]{"http"});
         beanConfig.setHost("localhost:8080");
         beanConfig.setBasePath("/reservation/api");
         beanConfig.setResourcePackage("rest");
         beanConfig.setPrettyPrint(true);
-        beanConfig.setScan();
+        beanConfig.setScan();*/
     }
 }
