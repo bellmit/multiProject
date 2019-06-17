cd2a2e12bb4c7f855c8f65c141b94a8c81b4addf
Unknown <ldamhuis@hotmail.com>
2019-06-17 14:02:41 +0200

    Removed stacktrace

--- deliver/src/main/java/messaging/SimulationMessageCleanUp.java
+++ deliver/src/main/java/messaging/SimulationMessageCleanUp.java
@@ -4,8 +4,12 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SimulationMessageCleanUp {
+
+    private static final Logger LOGGER = Logger.getLogger(SimulationMessageCleanUp.class.getName());
 
     public static void clean(Channel channel, String finalcoord, Connection connection){
         try {
@@ -13,7 +17,7 @@ public class SimulationMessageCleanUp {
             channel.queueDelete("deadletter" + finalcoord);
             connection.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE,"Something went wrong in cleanup: "+ e.getMessage());
         }
 
     }
